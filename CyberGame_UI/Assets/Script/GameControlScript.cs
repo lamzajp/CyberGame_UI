f5413d03e1c208a5341b6dfefd499b3651e369fd
ryosios <ryosioss@gmail.com>
2020-06-25 13:18:25 +0900

    シーン遷移スクリプトとフェード追加

---
+++ CyberGame_UI/Assets/Script/GameControlScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControlScript : MonoBehaviour
+{
+    [SerializeField] GameObject Fade_start;
+    GameObject MainCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainCanvas = GameObject.Find("MainMenu_Canvas");
+        if(MainCanvas == null)
+        {
+            MainCanvas = GameObject.Find("Shop_Canvas");
+
+        }
+        GameObject obj = (GameObject)Instantiate(Fade_start);
+        obj.transform.SetParent(MainCanvas.transform, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
